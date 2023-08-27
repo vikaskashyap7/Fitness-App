7185f82063821ec65474eb83da3c2836f93757d7
vikaskashyap7 <vikaskashyap1334@gmail.com>
2023-08-27 22:06:05 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Box} from "@mui/material";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home  from "./pages/Home";
+
+import Footer from "./components/Footer";
+import ExerciseDetail from "./pages/ExerciseDetail";
+
+function App() {
+  return (
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
+        
+      </Routes>
+      <Footer/> 
+    </Box>
+  );
+}
+
+export default App;

a94a1393c2ae3a5583d50c5edb61d2b3b7ae75e0
vikaskashyap7 <126238976+vikaskashyap7@users.noreply.github.com>
2023-12-21 20:26:46 +0530

    Update Footer.jsx second

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
 {/*       <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} /> */}
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by Vikas Kashyap</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made by 😁 Vikas Kashyap</Typography>
   </Box>
 );
 
